8f9f7aa4f479fd5ab5d4a3a18d4b39c3ab8c943a
Mehy18 <mihailuminosul@gmail.com>
2025-01-03 20:40:58 +0200

    Create scroll-title.js

---
+++ jss/scroll-title.js
@@ -0,0 +1,14 @@
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show-title');
+		} else {
+			entry.target.classList.remove('show-title');
+		}
+	});
+});
+const hiddenElements = document.querySelectorAll('.hidden-title');
+ hiddenElements.forEach((el) => observer.observe(el));
+
